99490b5d3862114b1265bf1c8f8c28b6b486aa3a
Konstantin Dunaev <shop.overled@gmail.com>
2021-08-15 18:38:13 +0300

    Добавил тест и json schema

---
+++ src/test/java/ru/netology/PostRequestTest.java
@@ -0,0 +1,20 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class PostRequestTest {
+    @Test
+    void shouldSendRequest(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("echo.schema.json"));
+    }
+}
